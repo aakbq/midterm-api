62e84c4aac28f2db2ebba53e6fcee656b9b09614
aakbq <97110798+aakbq@users.noreply.github.com>
2022-05-03 17:19:54 +0600

    no comments, only emotions

--- server.js
+++ server.js
@@ -8,14 +8,28 @@ const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Database Connected Successfully!!");
+}).catch(err => {
+    console.log('Could not connect to the database', err);
+    process.exit();
+});
+
 app.use(express.static('public'))
 
-
+app.use("/user", require("./routes/User"))
 app.use("/", require("./routes/index.js"))
 app.use("/about", require("./routes/about.js"))
 app.use("/products", require("./routes/products.js"))
 app.use("/login", require("./routes/login.js"))
 app.use("/signup", require("./routes/signup.js"))
+app.use("/profile", require("./routes/profile.js"))
+
 
 app.listen(port, () =>
     console.log(`App listening at http://localhost:${port}`)
